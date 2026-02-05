21c23c4302644f93f3d17a6935392905f4a993b8
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-05 21:34:57 +0500

    updating toasts

--- src/components/common/Dashboard/Toasts.jsx
+++ src/components/common/Dashboard/Toasts.jsx
@@ -2,7 +2,7 @@ import { useToast } from "../../../hooks/use-toast.js"
 import { Button } from "@/components/ui/button"
 import { ToastAction } from "@/components/ui/toast"
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
 import { HandlePostHREmployees } from "../../../redux/Thunks/HREmployeesThunk.js"
 export const FormSubmitToast = ({ formdata }) => {
     const { toast } = useToast()
@@ -14,24 +14,29 @@ export const FormSubmitToast = ({ formdata }) => {
         dispatch(HandlePostHREmployees({ apiroute: "ADDEMPLOYEE", data: formdata })) 
     }
 
-    // const DisplayToast = () => {
-    //     if (HREmployeesState.error.status) {
-    //         return toast({
-    //             variant: "destructive",
-    //             title: "Uh oh! Something went wrong.",
-    //             description: `${HREmployeesState.error.message}`,
-    //             // action: <ToastAction altText="Try again">Try again</ToastAction>,
-    //         })
-    //     } else if (HREmployeesState.fetchData) {
-    //         return toast({
-    //             title: <p className="text-xl m-1">Success!</p>,
-    //             description: <div className="flex justify-center items-center gap-2">
-    //                 <img src="../../src/assets/HR-Dashboard/correct.png" alt="" className="w-8" />
-    //                 <p className="font-bold">Employee added successfully.</p>
-    //             </div>,
-    //         })
-    //     }
-    // }
+    const DisplayToast = () => {
+        if (HREmployeesState.error.status) {
+            return toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: `${HREmployeesState.error.message}`,
+            })
+        } else if (HREmployeesState.fetchData) {
+            return toast({
+                title: <p className="text-xl m-1">Success!</p>,
+                description: <div className="flex justify-center items-center gap-2">
+                    <img src="../../src/assets/HR-Dashboard/correct.png" alt="" className="w-8" />
+                    <p className="font-bold">Employee added successfully.</p>
+                </div>,
+            })
+        }
+    }
+
+    useEffect(() => {
+        if (HREmployeesState.error.status || HREmployeesState.fetchData) {
+            DisplayToast()
+        }
+    }, [HREmployeesState.error.status, HREmployeesState.fetchData])
 
     console.log(HREmployeesState, "This is the HR plus Employees State")
     return (
@@ -40,19 +45,6 @@ export const FormSubmitToast = ({ formdata }) => {
                 variant="outline"
                 onClick={() => {
                     SubmitFormData()
-                    // HREmployeesState.error.status ? toast({
-                    //     variant: "destructive",
-                    //     title: "Uh oh! Something went wrong.",
-                    //     description: `${HREmployeesState.error.message}`,
-                    //     // action: <ToastAction altText="Try again">Try again</ToastAction>,
-                    // }) : null
-                    // HREmployeesState.fetchData ? toast({
-                    //     title: <p className="text-xl m-1">Success!</p>,
-                    //     description: <div className="flex justify-center items-center gap-2">
-                    //         <img src="../../src/assets/HR-Dashboard/correct.png" alt="" className="w-8" />
-                    //         <p className="font-bold">Employee added successfully.</p>
-                    //     </div>,
-                    // }) : null
                 }}
                 className="bg-blue-800 border-2 border-blue-800 px-4 py-2 text-white font-bold rounded-lg hover:bg-white hover:text-blue-800"
             >
