d8e0fe42a11d4fb3dd686147fb3d09e2f6304520
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-08 23:26:11 +0500

    .

--- src/components/common/Dashboard/Toasts.tsx
+++ src/components/common/Dashboard/Toasts.tsx
@@ -1,6 +1,6 @@
-import { useDispatch } from "react-redux"
-import { HandleDeleteHREmployees } from "../../../redux/Thunks/HREmployeesThunk"
-import { HandleDeleteHRDepartments } from "../../../redux/Thunks/HRDepartmentPageThunk"
+// import { useDispatch } from "react-redux"
+// import { HandleDeleteHREmployees } from "../../../redux/Thunks/HREmployeesThunk"
+// import { HandleDeleteHRDepartments } from "../../../redux/Thunks/HRDepartmentPageThunk"
 
 export const AddEmployeesDialogBox = () => {
     return (
@@ -9,15 +9,6 @@ export const AddEmployeesDialogBox = () => {
 }
 
 export const RemoveEmployeeFromDepartmentDialogBox = () => {
-    const dispatch = useDispatch()
-
-    // const RemoveEmployee = (EMID: string) => {
-    //     dispatch(HandleDeleteHRDepartments({ apiroute: "DELETE", data: { departmentID: "", employeeIDArray: [EMID], action: "delete-employee" } }) as any)
-    // }
-
-    return (
-        <div>Remove Employee Dialog</div>
-    )
 
     return (
         <div>Remove Employee Dialog</div>
@@ -25,10 +16,6 @@ export const RemoveEmployeeFromDepartmentDialogBox = () => {
 }
 
 export const DeleteEmployeeDialogBox = () => {
-    const dispatch = useDispatch()
-    // const DeleteEmployee = (EMID: string) => {
-    //     dispatch(HandleDeleteHREmployees({ apiroute: `DELETE.${EMID}` }) as any)
-    // }
 
     return (
         <div>Delete Employee Dialog</div>
