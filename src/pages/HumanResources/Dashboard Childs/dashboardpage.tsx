eb49694c9e3bafdcf6eb2923ca9c8c9de610a5b0
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-08 20:21:46 +0500

    removing errors

--- src/pages/HumanResources/Dashboard Childs/dashboardpage.tsx
+++ src/pages/HumanResources/Dashboard Childs/dashboardpage.tsx
@@ -1,10 +1,10 @@
-import { KeyDetailBoxContentWrapper } from "../../../components/common/Dashboard/contentwrappers";
+import { ContentWrapper } from "../../../components/common/Dashboard/contentwrappers";
 import { SalaryChart } from "../../../components/common/Dashboard/salarychart";
 import { DataTable } from "../../../components/common/Dashboard/datatable";
 import { useEffect } from "react";
 import { HandleGetDashboard } from "../../../redux/Thunks/DashboardThunk";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+
 import { Loading } from "../../../components/common/loading.tsx";
 
 export const HRDashboardPage = () => {
@@ -46,10 +46,26 @@ export const HRDashboardPage = () => {
 
   return (
     <>
-      <KeyDetailBoxContentWrapper imagedataarray={DataArray} data={DashboardState.data} />
+      <ContentWrapper title="Dashboard Overview">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {DataArray.map((item, index) => (
+            <div key={index} className="bg-white p-4 rounded-lg shadow">
+              <img src={item.image} alt={item.dataname} className="w-16 h-16 mx-auto mb-2" />
+              <h3 className="text-center font-semibold capitalize">{item.dataname}</h3>
+            </div>
+          ))}
+        </div>
+      </ContentWrapper>
       <div className="salary-notices-container h-3/4 grid min-[250px]:grid-cols-1 lg:grid-cols-2 min-[250px]:gap-3 xl:gap-3">
-        <SalaryChart balancedata={DashboardState.data} />
-        <DataTable noticedata={DashboardState.data} />
+        <SalaryChart 
+          data={DashboardState.data?.salaryData || []} 
+          title="Salary Overview" 
+          description="Monthly salary distribution" 
+        />
+        <DataTable 
+          data={DashboardState.data?.notices || []} 
+          columns={[{ key: "title", label: "Title" }, { key: "description", label: "Description" }]} 
+        />
       </div>
     </>
   );
